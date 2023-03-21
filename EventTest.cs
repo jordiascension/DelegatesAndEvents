353b75e25146e7b5a7e3422f45a7d9683d649492
jordiascension <jordiascension@enterpriseconsulting.es>
2023-03-21 18:03:46 +0100

    Added Events and delegates example

---
+++ EventTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class EventTest
+    {
+        private int value;
+
+        public event NumManipulationHandler ChangeNum;
+
+        protected virtual void OnNumChanged()
+        {
+            if (ChangeNum != null)
+            {
+                Console.WriteLine("Event fired!");
+                ChangeNum();
+            }
+            else
+            {
+                Console.WriteLine("No Event fired!");
+            }
+        }
+
+        public EventTest(int n)
+        {
+            SetValue(n);
+        }
+
+        public void SetValue(int n)
+        {
+            if (value != n)
+            {
+                value = n;
+                OnNumChanged();
+            }
+        }
+    }
+}
---
+++ MainEventClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class MainEventClass
+    {
+        static void Main(string[] args)
+        {
+            EventTest e = new EventTest(5);
+            e.ChangeNum += new NumManipulationHandler(EventExecuted);
+            e.SetValue(7);
+            e.SetValue(11);
+            Console.ReadKey();
+        }
+
+        public static  void EventExecuted()
+        {
+            Console.WriteLine("Method executed via event");
+        }
+    }
+}
