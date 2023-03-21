638235a276aa0e71733850a3e15be6386f062b86
jordiascension <jordiascension@enterpriseconsulting.es>
2023-03-21 17:38:01 +0100

    Added delegates and multicastdelegates examples

---
+++ MultiCastDelegateStartup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class MultiCastDelegateStartup
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+
+        static void Main(string[] args)
+        {
+            NumberChanger nc1 = new NumberChanger(AddNum);
+
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            NumberChanger nc3 = nc1;
+            nc3 += nc2;
+
+            Console.WriteLine("The MultNum returns " + nc3(10).ToString());
+            
+        }
+    }
+}
---
+++ MyFirstDelegateStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class MyFirstDelegateStartup
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+
+        static void Main(string[] args)
+        {
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            Console.WriteLine("The AddNum returns " + nc1(10).ToString());
+
+            NumberChanger nc2 = new NumberChanger(MultNum);
+            Console.WriteLine("The MultNum returns " + nc2(10).ToString());
+        }
+    }
+}
